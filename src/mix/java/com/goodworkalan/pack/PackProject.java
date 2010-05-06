8c63aff2c68a4702e34ae3dd8e877ac432c4dfa0
Alan Gutierrez <alan@blogometer.com>
2010-05-06 03:54:09 -0500

    Updated for new Mix build.

--- src/mix/java/com/goodworkalan/pack/PackProject.java
+++ src/mix/java/com/goodworkalan/pack/PackProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.pack.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,19 +9,19 @@ public class PackProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/pack/0.1"))
+                .produces("com.github.bigeasy.pack/pack/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.goodworkalan/region/0.1"))
-                        .artifact(new Artifact("com.goodworkalan/sheaf/0.1"))
-                        .artifact(new Artifact("com.goodworkalan/vetiver/0.1"))
+                        .include("com.github.bigeasy.region/region/0.1")
+                        .include("com.github.bigeasy.sheaf/sheaf/0.1")
+                        .include("com.github.bigeasy.vetiver/vetiver/0.1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
-                        .artifact(new Artifact("args4j/args4j/2.0.8"))
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .include("org.mockito/mockito-core/1.6")
+                        .include("args4j/args4j/2.0.8")
+                        .include("org.testng/testng-jdk15/5.10")
                         .end()
                     .end()
                 .end()
