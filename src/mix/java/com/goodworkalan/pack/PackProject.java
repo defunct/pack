5a7f8ddb126dc760b7e43208dd1f7f852dee6dd1
Alan Gutierrez <alan@blogometer.com>
2010-05-20 23:51:02 -0500

    Mix Project is now an interface.

--- src/mix/java/com/goodworkalan/pack/PackProject.java
+++ src/mix/java/com/goodworkalan/pack/PackProject.java
@@ -4,25 +4,29 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class PackProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Pack.
+ *
+ * @author Alan Gutierrez
+ */
+public class PackProject implements ProjectModule {
+    /**
+     * Build the project definition for Pack.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.pack/pack/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.region/region/0.1")
-                        .include("com.github.bigeasy.sheaf/sheaf/0.1")
-                        .include("com.github.bigeasy.vetiver/vetiver/0.1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.mockito/mockito-core/1.6")
-                        .include("args4j/args4j/2.0.8")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.region/region/0.1")
+                    .production("com.github.bigeasy.sheaf/sheaf/0.1")
+                    .production("com.github.bigeasy.vetiver/vetiver/0.1")
+                    .development("org.mockito/mockito-core/1.6")
+                    .development("args4j/args4j/2.0.8")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
